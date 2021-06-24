8be09895fa39f01458c15258138404da1b940bb8
victor <asdasd>
2021-06-25 00:08:23 +0200

    Resolucion Ejercicio 2 : parte 3

--- Ejercicio 2/ejercicio_2.py
+++ Ejercicio 2/ejercicio_2.py
@@ -12,14 +12,14 @@ class Persona(object):
     def __init__ (self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
-    def presentation(self):
+    def presentacion(self):
         print(f"Hola! Soy {self.nombre} y tengo {self.edad} años")
 
 class Trabajador(Persona):
-    """Clase que representa a un Supervisor"""
+    """Clase que representa a un Trabajador"""
 
     def __init__(self, nombre, edad, departamento, puesto):
-        """Constructor de clase Supervisor"""
+        """Constructor de clase Trabajador"""
 
         # Invoca al constructor de clase Persona
         Persona.__init__(self, nombre, edad)
@@ -28,12 +28,23 @@ class Trabajador(Persona):
         self.departamento = departamento
         self.puesto = puesto
 
-    def presentation(self):
-            Persona.presentation(self)
+    def presentacion(self):
+            Persona.presentacion(self)
             print(f"Trabajo en el departamento: {self.departamento} y tengo el puesto {self.puesto}")
 
 nombre = 'Alberto'
 persona_1 = Persona(nombre, 20)
 trabajador_1 = Trabajador(nombre,20,"BigData","Data Engeneering")
-persona_1.presentation()
-trabajador_1.presentation()
+persona_1.presentacion()
+trabajador_1.presentacion()
+
+######################################################################################################################
+# Ejercicio 2 apartado 3
+#¿Qué diferencia hay entre self.nombre y la variable llamada nombre? Puedes incluirlo como comentario en el código.
+#
+#La diferencia es que self.nombre es el valor que tiene el contructor padre pàra esa variable
+# y la variable nombre es el valor que se esta pasando a el contructor
+# en este caso cuando se llama a el contructor Persona el nomber es Alberto
+# y self.nombre tomara el valor Alberto ya que el objeto Persona tiene nombre y edad
+#
+######################################################################################################################
